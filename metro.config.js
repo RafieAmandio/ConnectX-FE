01abc8c7d23c00a815b7c22116fd599a85ad7378
dwiki <dwikis17@gmail.com>
2026-06-01 13:50:08 +0700

    feat: configure Metro to support wasm assets and enable cross-origin isolation headers

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,16 @@ const { withNativewind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.resolver.assetExts.push('wasm');
+
+config.server.enhanceMiddleware = (middleware) => {
+  return (req, res, next) => {
+    res.setHeader('Cross-Origin-Embedder-Policy', 'credentialless');
+    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+    return middleware(req, res, next);
+  };
+};
+
 module.exports = withNativewind(config, {
   inlineVariables: false,
 });
